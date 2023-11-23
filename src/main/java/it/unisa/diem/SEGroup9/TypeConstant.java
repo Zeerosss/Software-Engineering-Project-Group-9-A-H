2f341d77fdbeb508194de264cfc18c6ac468743d
federiconicodemo <f.nicodemo3@studenti.unisa.it>
2023-11-23 15:36:03 +0100

    extra 1 adding action constants

--- src/main/java/it/unisa/diem/SEGroup9/TypeConstant.java
+++ src/main/java/it/unisa/diem/SEGroup9/TypeConstant.java
@@ -7,13 +7,21 @@ import java.util.Map;
 
 public class TypeConstant {
     public static final Map<Integer, String> TRIGGERTYPES_CONSTANTS;
+    public static final Map<Integer, String> ACTIONTYPES_CONSTANTS;
     static {
         // Creazione del dizionario
-        Map<Integer, String> map = new HashMap<>();
-        map.put(0, "timetrigger");
+        Map<Integer, String> map1= new HashMap<>();
+        Map<Integer, String> map2= new HashMap<>();
+
+        map1.put(0, "timetrigger");
+        
+        map2.put(0,"audioaction");
+        map2.put(1,"messageaction");
+
        
         
-        TRIGGERTYPES_CONSTANTS = Collections.unmodifiableMap(map);
+        TRIGGERTYPES_CONSTANTS = Collections.unmodifiableMap(map1);
+        ACTIONTYPES_CONSTANTS = Collections.unmodifiableMap(map2);
     }
 
 }
